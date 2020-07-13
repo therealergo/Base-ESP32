1326551eecf723a1298e0636f9a31b7a8c78db17
Kienan <kienan@saucybytes.com>
2020-07-13 02:12:53 -0500

    Improve robustness of documentation-open command.

--- tools/documentation_open.py
+++ tools/documentation_open.py
@@ -9,9 +9,17 @@ if not os.path.isdir("./docs"):
     if not result is 0:
         sys.exit(result)
 
+# Select documentation main page to open based on what's availible
+docfile = os.path.join(".", "docs", "html", "annotated.html")
+if not os.path.isfile(docfile):
+    docfile = os.path.join(".", "docs", "html", "index.html")
+    if not os.path.isfile(docfile):
+        print("Cound not find documention file to open!")
+        sys.exit(-1)
+
 # Open 'annotated' page as main page of documentation
 platformIdentifier = platform.system().lower()
 if platformIdentifier.startswith('mac') or platformIdentifier.startswith('darwin'):
-    sys.exit(os.system("open " + os.path.join(".", "docs", "html", "annotated.html")))
+    sys.exit(os.system("open " + docfile))
 else:
-    sys.exit(os.system(os.path.join(".", "docs", "html", "annotated.html")))
+    sys.exit(os.system(docfile))
