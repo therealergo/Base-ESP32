1f0ca9cdd437d4b2b53a8ce2791d33d57088ef2b
Kienan <kienan@saucybytes.com>
2020-10-04 16:17:25 -0500

    Improved menuconfig internal update logic.

--- tools/menu_config.py
+++ tools/menu_config.py
@@ -13,7 +13,5 @@ result = os.system('python ./tools/build_target.py menuconfig')
 # Copy generated 'sdkconfig' file into '.vscode' directory
 # This lets VS Code load in the sdkconfig defines properly
 # Note that any run to MenuConfig will regenerate this file
-if result == 0:
-    shutil.copy('./build/config/sdkconfig.h', './.vscode')
-else:
-    sys.exit(result)
+print('Loading updated sdkconfig.h file into VS Code...')
+shutil.copy('./build/config/sdkconfig.h', './.vscode')
